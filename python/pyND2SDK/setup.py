1df2696165422de32d310be822ea71565ac9a829
Aaron Ponti <aarpon@bsse.ethz.ch>
2017-05-22 10:46:56 +0200

    Fix fully qualified name mismatch warning.

--- python/pyND2SDK/setup.py
+++ python/pyND2SDK/setup.py
@@ -6,8 +6,9 @@ from numpy import get_include
 from pyND2SDK.settings import SDKIncludePath, SDKLibPath
 
 setup(
+    name="pyND2SDK",
     ext_modules=cythonize([
-        Extension("nd2Reader", ["nd2Reader.pyx", "nd2Reader_helper.c"],
+        Extension("pyND2SDK.nd2Reader", ["nd2Reader.pyx", "nd2Reader_helper.c"],
                   include_dirs=[SDKIncludePath, get_include()],
                   libraries=['v6_w32_nd2ReadSDK'],
                   library_dirs=[SDKLibPath]
