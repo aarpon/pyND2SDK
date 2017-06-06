3d589e45699bb57a5f0b9595e09ec96b983b61f1
Aaron Ponti <aarpon@bsse.ethz.ch>
2017-06-06 17:07:35 +0200

    Change from distutils to setuptools.

--- python/setup.py
+++ python/setup.py
@@ -1,27 +1,28 @@
-from distutils.core import setup
-from distutils.extension import Extension
+from setuptools import setup
+from setuptools.extension import Extension
 from Cython.Build import cythonize
 from numpy import get_include
 
 from pyND2SDK.settings import SDKIncludePath, SDKLibPath
 
 setup(
-    name="pyND2SDK",
+    name='pyND2SDK',
     description='Python wrapper for the Nikon ND2 SDK C library.',
     author='Aaron Ponti',
+    url='http://www.scs2.net',
     author_email='aaron.ponti@bsse.ethz.ch',
-    packages=["", "tests"],
-    package_dir={'': 'pyND2SDK',
-                 'tests': 'pyND2SDK/tests'},
-    package_data={'tests': ['pyND2SDK/tests/files/*.nd2']},
+    packages=["pyND2SDK"],
+    package_dir={'pyND2SDK': 'pyND2SDK'},
+    package_data={'pyND2SDK': ['pyND2SDK/test/files/*.nd2']},
     version='0.0.1',
     ext_modules=cythonize([
-        Extension("nd2Reader", ["pyND2SDK/nd2Reader.pyx",
-                                 "pyND2SDK/nd2Reader_helper.c"],
+        Extension("pyND2SDK.nd2Reader", ["pyND2SDK/nd2Reader.pyx",
+                                         "pyND2SDK/nd2Reader_helper.c"],
                   include_dirs=[SDKIncludePath, get_include()],
                   libraries=['v6_w32_nd2ReadSDK'],
                   library_dirs=[SDKLibPath]
                   )
     ]),
+    install_requires=['python_version>"3.4"'],
     requires=["Cython", "numpy"]
 )
